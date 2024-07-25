cd679641da02a53e7450b389491bceda9bdc115d
bgt-jchristopher <36265641+bgt-jchristopher@users.noreply.github.com>
2024-07-25 10:01:27 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include ("functions.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<?php create_header(""); ?>
+  </head>
+  <body>
+
+    <?php 
+		create_wrap("index"); 
+		create_nav("index"); 
+		create_hero("index");
+		create_welcome("index");
+		create_services("index");
+		create_stats("index");
+		create_faq("index");
+		create_blog("index");
+		create_consult("index");
+		create_footer("index");
+		create_scripts("index");
+	?>
+	
+
+  
+
+    
+  </body>
+</html>
