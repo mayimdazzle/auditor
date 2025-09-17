c2e696060ff34174d55722ecb9acf71453cc4535
Christopher Paul L <87556500+chrispaul07@users.noreply.github.com>
2025-09-17 13:25:10 +0530

    fix: Service page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
+import Services from './pages/Services'
 import Contact from './pages/Contact'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfServices from './pages/TermsOfServices'
@@ -17,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/services" element={<TermsOfServices />} />
+            <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="/termsofservices" element={<TermsOfServices />} />
